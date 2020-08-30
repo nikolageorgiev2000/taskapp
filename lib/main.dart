7186ac8b0fe4fe89deef2f0d923c8700230d73f6
nikolag <nikola.georgiev2000@hotmail.com>
2020-08-30 02:30:40 +0200

    Implemented basic listing with scroll position stored when switching between pages. Added Firebase initialization.

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,204 @@
 import 'package:flutter/material.dart';
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  // Set default `_initialized` and `_error` state to false
+  bool _initialized = false;
+  bool _error = false;
+
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    // Show error message if initialization failed
+    if (_error) {
+      return MaterialApp(
+          home: Dialog(
+        child: Text("ERROR LOADING DATA!"),
+      ));
+    }
+
+    // Show a loader until FlutterFire is initialized
+    if (!_initialized) {
+      return MaterialApp(
+          home: Dialog(
+        child: Text("LOADING DATA..."),
+      ));
+    }
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'DoCenter Test',
+      home: MenuController(),
+      theme: ThemeData(primaryColor: Colors.white),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MenuController extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MenuControllerState createState() => _MenuControllerState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MenuControllerState extends State<MenuController> {
+  int _selectedIndex = 0;
+  final _bucket = PageStorageBucket();
 
-  void _incrementCounter() {
+  List<Widget> pages = [
+    TaskList(PageStorageKey("Task Page Key")),
+    Text("Events"),
+    Text("Stats")
+  ];
+
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: AppBar(leading: Icon(Icons.ac_unit)),
+      //Use PageStorage to save the scroll offset using the ScrollController in TaskList
+      body: PageStorage(bucket: _bucket, child: pages[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.alarm_on), label: "Tasks"),
+          BottomNavigationBarItem(icon: Icon(Icons.today), label: "Events"),
+          BottomNavigationBarItem(icon: Icon(Icons.show_chart), label: "Stats"),
+        ],
+        iconSize: 30,
+        currentIndex: _selectedIndex,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.lightBlue,
+        onTap: _onItemTapped,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class TaskList extends StatefulWidget {
+  TaskList(Key key) : super(key: key);
+
+  @override
+  _TaskListState createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  ScrollController scrollController = ScrollController();
+
+  Widget createTaskCard(int index) {
+    return Card(
+      child: InkWell(
+          splashColor: Colors.blue.withAlpha(30),
+          onLongPress: () {
+            print('Card long-pressed.');
+          },
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // LEFT COLUMN
+                Expanded(
+                    child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Column(
+                          children: [Text(index.toString())],
+                          mainAxisSize: MainAxisSize.max,
+                        ))),
+                // RIGHT COLUMN
+                Align(
+                    alignment: Alignment.topRight,
+                    child: Column(
+                      children: [
+                        Align(
+                            alignment: Alignment.bottomRight,
+                            child: Row(
+                              children: [
+                                IconButton(
+                                    icon: Icon(Icons.play_arrow),
+                                    onPressed: null),
+                                Text("1:05")
+                              ],
+                            )),
+                        Row(
+                          children: [
+                            IconButton(
+                                icon: Icon(Icons.check), onPressed: null),
+                            Text("Complete?")
+                          ],
+                        )
+                      ],
+                    ))
+              ],
+            ),
+          )),
+      elevation: 3,
+      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+    );
+  }
+
+  //Create list of tasks
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        controller: scrollController,
+        /*Use itermCount (length of list) and itemExtent (height of an item)
+        for improved speed switching between tabs (rebuilding list) */
+        // itemCount: 100,
+        // itemExtent: 150,
+        itemBuilder: (context, index) {
+          return createTaskCard(index);
+        });
+  }
+}
+
+/*
+Task document format:
+int task-ID
+String name
+String description
+float timeDue epoch
+String location
+bool completed
+int event-ID
+
+enum? category
+float timeLastEdited (use most recent time as truth for everything except )
+*/
+
+/*TODO: figure out how to: 
+    animate list, 
+    add task, 
+    switch between viewing saved/editing task, 
+    add task from event page, add task button
+*/
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:taskapp/main.dart';
+import 'package:taskapp/main_OLD.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
